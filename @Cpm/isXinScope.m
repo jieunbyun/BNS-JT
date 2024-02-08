e81c42066f49282bb4e163dd1780f20520b0bd26
Hyeuk Ryu <dynaryu@gmail.com>
2024-02-09 10:55:27 +1100

    clean up

--- @Cpm/isXinScope.m
+++
@@ -1,9 +0,0 @@
-function flagXinScope = isXinScope( X,M )
-%{
-Determine if M includes X in the scope
-%}
-
-flagXinScope = false( size(M) );
-for xx = 1:length(X)
-    flagXinScope = flagXinScope | arrayfun( @(x) ismember(X(xx),x.variables),M );
-end
